e746c62026f923457100fab094a9d44658af1fae
Alexandre Filho <alexfiofcasf@gmail.com>
2017-02-09 09:37:32 -0300

    mudança no develop

--- app/Http/Controllers/CandidatosController.php
+++ app/Http/Controllers/CandidatosController.php
@@ -34,7 +34,8 @@ class CandidatosController extends Controller {
         return view('candidatos.cadastro', $dados);
     }
     
-    //Carrega os dados de candidato de forma detalhada
+    //
+    //Carrega os dados de candidato de forma detalhad
      public function carregarViewConsultar($id) {
         $criterios['id'] = $id;
         $dados['candidato'] = 
